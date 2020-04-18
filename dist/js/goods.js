ebc89b48236c603d3bb7bbc65def5a3a4e3ded49
Vladislav1703 <lokotlt@gmail.com>
2020-04-18 22:13:32 +0400

    solve minor errors

--- dist/js/goods.js
+++ dist/js/goods.js
@@ -3,121 +3,121 @@ let goodsSets = {
         "name" : "Сет №10",
         "cost" : 2050,
         "description" : "72 шт. | 1560 г. | 1700 Kkal",
-        "image" : "url(../img/sets_1.png)" 
+        "image" : "url(img/sets_1.png)" 
     },
     "10102" : {
         "name" : "Сет №8",
         "cost" : 1590,
         "description" : "24 шт. | 960 г. | 800 Kkal",
-        "image" : "url(../img/sets_2.png)"
+        "image" : "url(img/sets_2.png)"
     },
     "10103" : {
         "name" : "Минисет №5",
         "cost" : 510,
         "description" : "14 шт. | 450 г. | 400 Kkal",
-        "image" : "url(../img/sets_3.png)"
+        "image" : "url(img/sets_3.png)"
     },
     "10104" : {
         "name" : "Сет №6",
         "cost" : 400,
         "description" : "16 шт. | 460 г. | 490 Kkal",
-        "image" : "url(../img/sets_4.png)"
+        "image" : "url(img/sets_4.png)"
     },
     "10105" : {
         "name" : "Сет №10",
         "cost" : 2050,
         "description" : "72 шт. | 1560 г. | 1700 Kkal",
-        "image" : "url(../img/sets_1.png)" 
+        "image" : "url(img/sets_1.png)" 
     },
     "10106" : {
         "name" : "Сет №8",
         "cost" : 1590,
         "description" : "24 шт. | 960 г. | 800 Kkal",
-        "image" : "url(../img/sets_2.png)"
+        "image" : "url(img/sets_2.png)"
     },
     "10107" : {
         "name" : "Минисет №5",
         "cost" : 510,
         "description" : "14 шт. | 450 г. | 400 Kkal",
-        "image" : "url(../img/sets_3.png)"
+        "image" : "url(img/sets_3.png)"
     },
     "10108" : {
         "name" : "Сет №6",
         "cost" : 400,
         "description" : "16 шт. | 460 г. | 490 Kkal",
-        "image" : "url(../img/sets_4.png)"
+        "image" : "url(img/sets_4.png)"
     },
     "10109" : {
         "name" : "Сет №10",
         "cost" : 2050,
         "description" : "72 шт. | 1560 г. | 1700 Kkal",
-        "image" : "url(../img/sets_1.png)" 
+        "image" : "url(img/sets_1.png)" 
     },
     "10110" : {
         "name" : "Сет №8",
         "cost" : 1590,
         "description" : "24 шт. | 960 г. | 800 Kkal",
-        "image" : "url(../img/sets_2.png)"
+        "image" : "url(img/sets_2.png)"
     },
     "10111" : {
         "name" : "Минисет №5",
         "cost" : 510,
         "description" : "14 шт. | 450 г. | 400 Kkal",
-        "image" : "url(../img/sets_3.png)"
+        "image" : "url(img/sets_3.png)"
     },
     "10112" : {
         "name" : "Сет №6",
         "cost" : 400,
         "description" : "16 шт. | 460 г. | 490 Kkal",
-        "image" : "url(../img/sets_4.png)"
+        "image" : "url(img/sets_4.png)"
     },
     "10113" : {
         "name" : "Сет №10",
         "cost" : 2050,
         "description" : "72 шт. | 1560 г. | 1700 Kkal",
-        "image" : "url(../img/sets_1.png)" 
+        "image" : "url(img/sets_1.png)" 
     },
     "10114" : {
         "name" : "Сет №8",
         "cost" : 1590,
         "description" : "24 шт. | 960 г. | 800 Kkal",
-        "image" : "url(../img/sets_2.png)"
+        "image" : "url(img/sets_2.png)"
     },
     "10115" : {
         "name" : "Минисет №5",
         "cost" : 510,
         "description" : "14 шт. | 450 г. | 400 Kkal",
-        "image" : "url(../img/sets_3.png)"
+        "image" : "url(img/sets_3.png)"
     },
     "10116" : {
         "name" : "Сет №6",
         "cost" : 400,
         "description" : "16 шт. | 460 г. | 490 Kkal",
-        "image" : "url(../img/sets_4.png)"
+        "image" : "url(img/sets_4.png)"
     },
     "10117" : {
         "name" : "Сет №10",
         "cost" : 2050,
         "description" : "72 шт. | 1560 г. | 1700 Kkal",
-        "image" : "url(../img/sets_1.png)" 
+        "image" : "url(img/sets_1.png)" 
     },
     "10118" : {
         "name" : "Сет №8",
         "cost" : 1590,
         "description" : "24 шт. | 960 г. | 800 Kkal",
-        "image" : "url(../img/sets_2.png)"
+        "image" : "url(img/sets_2.png)"
     },
     "10119" : {
         "name" : "Минисет №5",
         "cost" : 510,
         "description" : "14 шт. | 450 г. | 400 Kkal",
-        "image" : "url(../img/sets_3.png)"
+        "image" : "url(img/sets_3.png)"
     },
     "10120" : {
         "name" : "Сет №6",
         "cost" : 400,
         "description" : "16 шт. | 460 г. | 490 Kkal",
-        "image" : "url(../img/sets_4.png)"
+        "image" : "url(img/sets_4.png)"
     },
 };
 
@@ -126,121 +126,121 @@ let goodsRolls = {
         "name" : "Кунсей батакон",
         "cost" : 260,
         "description" : "8 шт. | 250 г. | 350 Kkal",
-        "image" : "url(../img/rolls_1.png)"
+        "image" : "url(img/rolls_1.png)"
     },
     "10202" : {
         "name" : "Унаги диру",
         "cost" : 280,
         "description" : "8 шт. | 960 г. | 800 Kkal",
-        "image" : "url(../img/rolls_2.png)"
+        "image" : "url(/img/rolls_2.png)"
     },
     "10203" : {
         "name" : "Филадельфия",
         "cost" : 280,
         "description" : "8 шт. | 450 г. | 400 Kkal",
-        "image" : "url(../img/rolls_3.png)"
+        "image" : "url(img/rolls_3.png)"
     },
     "10204" : {
         "name" : "Мексиканский",
         "cost" : 290,
         "description" : "8 шт. | 460 г. | 490 Kkal",
-        "image" : "url(../img/rolls_4.png)"
+        "image" : "url(img/rolls_4.png)"
     },
     "10205" : {
         "name" : "Кунсей батакон",
         "cost" : 260,
         "description" : "8 шт. | 250 г. | 350 Kkal",
-        "image" : "url(../img/rolls_1.png)"
+        "image" : "url(img/rolls_1.png)"
     },
     "10206" : {
         "name" : "Унаги диру",
         "cost" : 280,
         "description" : "8 шт. | 960 г. | 800 Kkal",
-        "image" : "url(../img/rolls_2.png)"
+        "image" : "url(img/rolls_2.png)"
     },
     "10207" : {
         "name" : "Филадельфия",
         "cost" : 280,
         "description" : "8 шт. | 450 г. | 400 Kkal",
-        "image" : "url(../img/rolls_3.png)"
+        "image" : "url(img/rolls_3.png)"
     },
     "10208" : {
         "name" : "Мексиканский",
         "cost" : 290,
         "description" : "8 шт. | 460 г. | 490 Kkal",
-        "image" : "url(../img/rolls_4.png)"
+        "image" : "url(img/rolls_4.png)"
     },
     "10209" : {
         "name" : "Кунсей батакон",
         "cost" : 260,
         "description" : "8 шт. | 250 г. | 350 Kkal",
-        "image" : "url(../img/rolls_1.png)"
+        "image" : "url(img/rolls_1.png)"
     },
     "10210" : {
         "name" : "Унаги диру",
         "cost" : 280,
         "description" : "8 шт. | 960 г. | 800 Kkal",
-        "image" : "url(../img/rolls_2.png)"
+        "image" : "url(img/rolls_2.png)"
     },
     "10211" : {
         "name" : "Филадельфия",
         "cost" : 280,
         "description" : "8 шт. | 450 г. | 400 Kkal",
-        "image" : "url(../img/rolls_3.png)"
+        "image" : "url(img/rolls_3.png)"
     },
     "10212" : {
         "name" : "Мексиканский",
         "cost" : 290,
         "description" : "8 шт. | 460 г. | 490 Kkal",
-        "image" : "url(../img/rolls_4.png)"
+        "image" : "url(img/rolls_4.png)"
     },
     "10213" : {
         "name" : "Кунсей батакон",
         "cost" : 260,
         "description" : "8 шт. | 250 г. | 350 Kkal",
-        "image" : "url(../img/rolls_1.png)"
+        "image" : "url(img/rolls_1.png)"
     },
     "10214" : {
         "name" : "Унаги диру",
         "cost" : 280,
         "description" : "8 шт. | 960 г. | 800 Kkal",
-        "image" : "url(../img/rolls_2.png)"
+        "image" : "url(img/rolls_2.png)"
     },
     "10215" : {
         "name" : "Филадельфия",
         "cost" : 280,
         "description" : "8 шт. | 450 г. | 400 Kkal",
-        "image" : "url(../img/rolls_3.png)"
+        "image" : "url(img/rolls_3.png)"
     },
     "10216" : {
         "name" : "Мексиканский",
         "cost" : 290,
         "description" : "8 шт. | 460 г. | 490 Kkal",
-        "image" : "url(../img/rolls_4.png)"
+        "image" : "url(img/rolls_4.png)"
     },
     "10217" : {
         "name" : "Кунсей батакон",
         "cost" : 260,
         "description" : "8 шт. | 250 г. | 350 Kkal",
-        "image" : "url(../img/rolls_1.png)"
+        "image" : "url(img/rolls_1.png)"
     },
     "10218" : {
         "name" : "Унаги диру",
         "cost" : 280,
         "description" : "8 шт. | 960 г. | 800 Kkal",
-        "image" : "url(../img/rolls_2.png)"
+        "image" : "url(img/rolls_2.png)"
     },
     "10219" : {
         "name" : "Филадельфия",
         "cost" : 280,
         "description" : "8 шт. | 450 г. | 400 Kkal",
-        "image" : "url(../img/rolls_3.png)"
+        "image" : "url(img/rolls_3.png)"
     },
     "10220" : {
         "name" : "Мексиканский",
         "cost" : 290,
         "description" : "8 шт. | 460 г. | 490 Kkal",
-        "image" : "url(../img/rolls_4.png)"
+        "image" : "url(img/rolls_4.png)"
     }
 };
 
